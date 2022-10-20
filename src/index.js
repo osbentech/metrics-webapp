dc4adeac12f59a4d6c0192743b54eb2268ef25c8
Benjamin Ejike <benmma13@gmail.com>
2022-10-20 15:33:25 +0400

    fetch, build coin data and style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+/* import App from './App'; */
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyCoin from './components/coin/coin';
+
+// App components
+import Navigation from './components/navbar';
+import MyCryptoCurrencies from './components/cryptohome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<MyCryptoCurrencies />} />
+          <Route path="/coin/:coinId" element={<MyCoin />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
